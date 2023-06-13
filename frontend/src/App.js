ea8f06b06922a4ae86bb444890ce91f47b47a30e
santhoshnov <shirobro009@gmail.com>
2023-06-13 18:05:36 +0530

    components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Home from './components/Home';
 import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
     AARAMBIKALAMA !
     <Home />
     <Projects />
+    <Skills />
+    <Contact />
     </h1>
   
   );
