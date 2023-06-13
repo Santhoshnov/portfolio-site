352d7581ada1da444756c9ac75d8accc193de1a8
santhoshnov <shirobro009@gmail.com>
2023-06-13 18:00:46 +0530

    components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Home from './components/Home';
+import Projects from './components/Projects';
 
 function App() {
   return (
+
    <h1 className="text-3xl font-bold underline">
     AARAMBIKALAMA !
+    <Home />
+    <Projects />
     </h1>
+  
   );
 }
 
