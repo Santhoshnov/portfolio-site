e9afb08980e8a76b53ad10a18d7b4c06b353f283
santhoshnov <shirobro009@gmail.com>
2023-06-13 18:17:05 +0530

    components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import Home from './components/Home';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
+import About from './components/About';
+import Achievements from './components/Achievements';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
     <Home />
     <Projects />
     <Skills />
+    <About/>
+    <Achievements/>
     <Contact />
     </h1>
   
