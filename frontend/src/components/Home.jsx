e6feca378ee57af51a7de62ac5c49613e546e7e4
santhoshnov <shirobro009@gmail.com>
2023-06-15 00:45:01 +0530

    homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSun, faMoon, faHome, faInfo, faBriefcase, faCogs, faPhone,faCode  } from "@fortawesome/free-solid-svg-icons";
+import { faSun, faMoon, faHome, faInfo, faBriefcase,faPhone,faCode,faLaptopCode} from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
 const [darkToggle, setDarkToggle] = React.useState(false);
@@ -13,62 +13,37 @@ const navbarColor = darkToggle ? "#303234" : "#FAFAFA";
 
 return (
     <div className={`min-h-screen ${darkToggle ? "bg-gray-900 text-white" : "bg-gray-100 text-gray-900"}`}>
-    <nav className={`${darkToggle ? "bg-gray-800" : "bg-white"} p-4 flex items-center justify-between`}
-        style={{ backgroundColor: navbarColor }} 
-      >
+    <nav className={`${darkToggle ? "bg-gray-800" : "bg-white"} p-4 flex items-center justify-between`} style={{ backgroundColor: navbarColor }} >
         <div className="flex items-center">
-          <h2
-            className={`text-2xl font-medium ${
-              darkToggle ? "text-white" : "text-gray-900"
-            } ${darkToggle ? "no-underline" : ""}`}
-          >
-            Santhosh
-          </h2>
+        <h2 className={`text-2xl font-medium bold ${darkToggle ? "text-white" : "text-gray-900"} ${darkToggle ? "no-underline" : ""}`}>
+        <FontAwesomeIcon icon={faLaptopCode} /> SANTHOSH
+        </h2>
+
         </div>
         <div className="ml-auto flex items-center">
-          <ul
-            className={`hidden md:flex space-x-4 ${
-              darkToggle ? "text-white" : "text-gray-900"
-            }`}
-          >
-           
-            <li>
-              <button
-                className="focus:outline-none"
-                onClick={() => console.log("Home button clicked")}
-              >
+          <ul className={`hidden md:flex space-x-20 ${darkToggle ? "text-white" : "text-gray-900"}`}>
+           <li>
+          <button className="text-2xl font-medium">
                 Home
-              </button>
-            </li>
-            <li>
-              <button
-                className="focus:outline-none"
-                onClick={() => console.log("About button clicked")}
-              >
+           </button>
+          </li>
+          <li>
+              <button className="text-2xl font-medium">
                 About
               </button>
             </li>
             <li>
-              <button
-                className="focus:outline-none"
-                onClick={() => console.log("Projects button clicked")}
-              >
+              <button className="text-2xl font-medium">
                 Projects
               </button>
             </li>
             <li>
-              <button
-                className="focus:outline-none"
-                onClick={() => console.log("Skills button clicked")}
-              >
+              <button className="text-2xl font-medium">
                 Skills
               </button>
             </li>
             <li>
-              <button
-                className="focus:outline-none"
-                onClick={() => console.log("Contact button clicked")}
-              >
+              <button className="text-2xl font-medium">
                 Contact
               </button>
             </li>
@@ -76,13 +51,13 @@ return (
           {darkToggle ? (
             <FontAwesomeIcon
               icon={faSun}
-              className="text-white cursor-pointer ml-4"
+              className="text-white cursor-pointer ml-20 text-2xl"
               onClick={toggleDarkMode}
             />
           ) : (
             <FontAwesomeIcon
               icon={faMoon}
-              className="text-gray-700 cursor-pointer ml-4"
+              className="text-gray-700 cursor-pointer ml-20 text-2xl"
               onClick={toggleDarkMode}
             />
           )}
@@ -90,10 +65,7 @@ return (
       </nav>
 
       {/* Mobile navbar */}
-      <nav
-  style={{ backgroundColor: navbarColor }}
-  className="p-4 fixed bottom-0 left-0 right-0 md:hidden flex flex-wrap justify-center"
->
+      <nav style={{ backgroundColor: navbarColor }} className="p-4 fixed bottom-0 left-0 right-0 md:hidden flex flex-wrap justify-center">
   <ul className={`flex flex-wrap justify-center space-x-4 ${darkToggle ? "text-white" : "text-gray-900"}`}>
     <li>
     <button className={`text-sm flex flex-col items-center ${darkToggle ? "text-white" : "text-gray-900"}`}>
@@ -120,7 +92,7 @@ return (
     <li>
     <button className={`text-sm flex flex-col items-center ${darkToggle ? "text-white" : "text-gray-900"}`}>
             <FontAwesomeIcon icon={faPhone} className="mb-1" />
-            Contacts
+            Contact
           </button>
     </li>
   </ul>
