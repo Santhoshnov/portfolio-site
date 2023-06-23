f32335864dbc54229813561113424293b73a752c
santhoshnov <shirobro009@gmail.com>
2023-06-23 17:11:57 +0530

    finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
-import About from './components/About';
-import Achievements from './components/Achievements';
+import Portfolio from './components/Portfolio';
+
 
 
 function App() {
   return (
 
-   <h1>
-   <Home />
-    <Navbar/>
-    <About/>
-    <Projects />
-    <Skills />
-    <Achievements/>
-    <Contact />
-    </h1>
+   <div>
+   <Portfolio/>
+    </div>
   
   );
 }
